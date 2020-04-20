f3041d00b4768d7112135a70993b1af6fda0a670
chy <chy@163.com>
2020-04-20 14:29:50 +0800

    init

---
+++ src/main/java/com/woniuxy/springboot/Myspringboot02Application.java
@@ -0,0 +1,13 @@
+package com.woniuxy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myspringboot02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myspringboot02Application.class, args);
+	}
+
+}
